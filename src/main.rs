fe18e0d95ccd76f6eee7102467148d97cf2a5401
muoscar <oscar.muhr@gmail.com>
2023-03-23 01:39:10 +0100

    Cleaned up the conde a little

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,37 @@
 use rand::Rng;
 use std::{thread, time};
 
-// use termion::event::Key;
-//use std::io::stdin;
-// use rustfft::{FftPlanner, num_complex::Complex};
+// TODO: - Implement exiting the program with 'q'.
+//       - Actually integrate audio?
 
-const FOREGROUND_BLACK:   &str = "\x1b[30m";
+const BAR_WIDTH:      usize = 75;
+const MAX_BAR_HEIGHT: u32   = 25;
+const FRAME_TIME:     u64   = 16_667;  // us
+const DATA_TIME:      u64   = 350_000; // us
+
+//const FOREGROUND_BLACK:   &str = "\x1b[30m";
 const FOREGROUND_RED:     &str = "\x1b[31m";
 const FOREGROUND_GREEN:   &str = "\x1b[32m";
 const FOREGROUND_YELLOW:  &str = "\x1b[33m";
-const FOREGROUND_BLUE:    &str = "\x1b[34m";
-const FOREGROUND_MAGENTA: &str = "\x1b[35m";
-const FOREGROUND_CYAN:    &str = "\x1b[36m";
-const FOREGROUND_WHITE:   &str = "\x1b[37m";
-const BACKGROUND_BLACK:   &str = "\x1b[40m";
-const BACKGROUND_RED:     &str = "\x1b[41m";
-const BACKGROUND_GREEN:   &str = "\x1b[42m";
-const BACKGROUND_YELLOW:  &str = "\x1b[43m";
-const BACKGROUND_BLUE:    &str = "\x1b[44m";
-const BACKGROUND_MAGENTA: &str = "\x1b[45m";
-const BACKGROUND_CYAN:    &str = "\x1b[46m";
-const BACKGROUND_WHITE:   &str = "\x1b[47m";
+//const FOREGROUND_BLUE:    &str = "\x1b[34m";
+//const FOREGROUND_MAGENTA: &str = "\x1b[35m";
+//const FOREGROUND_CYAN:    &str = "\x1b[36m";
+//const FOREGROUND_WHITE:   &str = "\x1b[37m";
+//const BACKGROUND_BLACK:   &str = "\x1b[40m";
+//const BACKGROUND_RED:     &str = "\x1b[41m";
+//const BACKGROUND_GREEN:   &str = "\x1b[42m";
+//const BACKGROUND_YELLOW:  &str = "\x1b[43m";
+//const BACKGROUND_BLUE:    &str = "\x1b[44m";
+//const BACKGROUND_MAGENTA: &str = "\x1b[45m";
+//const BACKGROUND_CYAN:    &str = "\x1b[46m";
+//const BACKGROUND_WHITE:   &str = "\x1b[47m";
 const RESET_COLOR:        &str = "\x1b[0m";
 const HIDE_CURSOR:        &str = "\x1b[?25l";
+const SHOW_CURSOR:        &str = "\x1b[?25h";
+const RESET:              &str = "\x1bc";
 const COLOR_SCALE:     &[&str] = &[FOREGROUND_GREEN,
                                    FOREGROUND_YELLOW,
                                    FOREGROUND_RED];
-
-fn print_frame_depr(bars: &[u32], max_height: u32) {
-    for i in 0..max_height {
-        for j in 0..bars.len() {
-            if bars[j] <= (max_height as u32)-i {
-                print!(" ");
-            } else {
-                print!("{}*", FOREGROUND_GREEN);
-            }
-        }
-        print!("\n    ");
-    }
-    for _i in 0..bars.len() {
-        print!("-");
-    }
-    print!("\n    ");
-    print!("    0Hz        10Hz        100Hz        1kHz        10kHz        100kHz    ");
-    print!("\n");
-}
 
 fn build_frame(bars: &[u32], max_height: u32) -> String {
     let mut frame: String = "".to_string();
@@ -60,11 +47,12 @@ fn build_frame(bars: &[u32], max_height: u32) -> String {
         } else {
             color = COLOR_SCALE[0]
         }
+        frame.push_str(color);
         for j in 0..bars.len() {
             if bars[j] <= (max_height as u32)-i {
                 frame.push_str(" ");
             } else {
-                frame.push_str(&format!("{}*", color).to_string());
+                frame.push_str("*");
             }
         }
         frame.push_str("\n    ");
@@ -76,41 +64,25 @@ fn build_frame(bars: &[u32], max_height: u32) -> String {
     frame.push_str("\n    ");
     frame.push_str("    0Hz        10Hz        100Hz        1kHz        10kHz        100kHz    ");
     frame.push_str("\n    ");
+    frame.push_str(SHOW_CURSOR);
     return frame;
 }
 
-fn print_frame(frame: String) {
-    print!("{}", frame);
-}
+fn print_frame(frame: String) { print!("{}", frame); }
 
-//struct bars {
-//    let bars: Vec<Vec<u32>>;
-//}
+fn exit() { print!("{}", RESET); }
+
 
 fn main() {
-//    let mut planner = FftPlanner::new();
-//    let fft = planner.plan_fft_forward(1234);
-//
-//    let mut buffer = vec![Complex{ re: 0.0f32, im: 0.0f32 }; 1234];
-//    fft.process(&mut buffer);
-//    let stdin = stdin();
-//        for evt in stdin.keys() {
-//            match evt {
-//                Key::Char('q') => println!("Nice!"),
-//            }
-//        }
-
-    const BAR_WIDTH:      usize = 75;
-    const MAX_BAR_HEIGHT: u32   = 25;
-    const FRAME_TIME:     u64   = 16_667; //16_667; // us
-    const DATA_TIME:      u64   = 300_000; // us
-
     let frame_time = time::Duration::from_micros(FRAME_TIME);
     let data_time = time::Duration::from_micros(DATA_TIME);
     let mut data_bars: [u32; BAR_WIDTH] = [0 as u32; BAR_WIDTH];
     let mut current_bars: [u32; BAR_WIDTH] = [0 as u32; BAR_WIDTH];
 
-    print_frame(build_frame(&current_bars, MAX_BAR_HEIGHT));
+    for i in 0..data_bars.len() {
+        data_bars[i] = rand::thread_rng().gen_range(0..MAX_BAR_HEIGHT);
+    }
+    print_frame(build_frame(&data_bars, MAX_BAR_HEIGHT));
 
     for _t in 0..100 {
         let data_instant = time::Instant::now();
@@ -136,9 +108,9 @@ fn main() {
                 }
             }
 
-            print!("{esc}c", esc = 27 as char);
-            //print!("\x1B[2J\n");
+            print!("{}", RESET);
             print_frame(build_frame(&current_bars, MAX_BAR_HEIGHT));
+
             if frame_instant.elapsed() <= frame_time {
                 thread::sleep(frame_time - frame_instant.elapsed());
             }
@@ -147,7 +119,6 @@ fn main() {
         for i in 0..data_bars.len() {
             data_bars[i] = rand::thread_rng().gen_range(0..MAX_BAR_HEIGHT);
         }
-
-        thread::sleep(frame_time);
     }
+    exit()
 }
